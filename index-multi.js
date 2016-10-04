68d9e15c1a03917d8f08a21cd3c10340ae591adf
christophe-g <christophe.geiser@e-smile.org>
2016-10-04 16:06:46 +0200

    first commit

---
+++ index-multi.js
@@ -0,0 +1,30 @@
+export * from "d3-selection"
+export * from "d3-selection-multi"
+export * from "d3-transition"
+export * from "d3-array"
+export * from "d3-brush"
+export * from "d3-drag"
+export * from "d3-geo"
+export * from "d3-path"
+export * from "d3-request"
+export * from "d3-time"
+export * from "d3-zoom"
+export * from "d3-ease"
+export * from "d3-pie"
+export * from "d3-shape"
+export * from "d3-color"
+export * from "d3-format"
+export * from "d3-interpolate"
+export * from "d3-scale"
+export * from "d3-svg-legend"
+
+/* Other Plugins */
+import legendColor from 'd3-svg-legend/src/color.js'
+import legendSize from 'd3-svg-legend/src/size.js'
+import legendSymbol from 'd3-svg-legend/src/symbol.js'
+
+export  {
+  legendColor,
+  legendSize,
+  legendSymbol
+};
