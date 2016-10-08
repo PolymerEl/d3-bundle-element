920d2b59f9dabb16eb8da49969a7834dec374a26
christophe-g <christophe.geiser@e-smile.org>
2016-10-08 15:51:27 +0200

    fix error in d3-element-multi.js

--- index-multi.js
+++ index-multi.js
@@ -7,17 +7,102 @@ export * from "d3-drag"
 export * from "d3-geo"
 export * from "d3-path"
 export * from "d3-request"
-export * from "d3-time"
 export * from "d3-zoom"
 export * from "d3-ease"
 export * from "d3-axis"
 export * from "d3-shape"
 export * from "d3-color"
-export * from "d3-format"
 export * from "d3-interpolate"
 export * from "d3-scale"
 export * from "d3-random"
 export * from "d3-svg-legend"
+
+export {
+  timeInterval,
+  timeMillisecond,
+  timeMilliseconds,
+  timeSecond,
+  timeSeconds,
+  timeMinute,
+  timeMinutes,
+  timeHour,
+  timeHours,
+  timeDay,
+  timeDays,
+  timeWeek,
+  timeWeeks,
+  timeSunday,
+  timeSundays,
+  timeMonday,
+  timeMondays,
+  timeTuesday,
+  timeTuesdays,
+  timeWednesday,
+  timeWednesdays,
+  timeThursday,
+  timeThursdays,
+  timeFriday,
+  timeFridays,
+  timeSaturday,
+  timeSaturdays,
+  timeMonth,
+  timeMonths,
+  timeYear,
+  timeYears,
+  utcMillisecond,
+  utcMilliseconds,
+  utcSecond,
+  utcSeconds,
+  utcMinute,
+  utcMinutes,
+  utcHour,
+  utcHours,
+  utcDay,
+  utcDays,
+  utcWeek,
+  utcWeeks,
+  utcSunday,
+  utcSundays,
+  utcMonday,
+  utcMondays,
+  utcTuesday,
+  utcTuesdays,
+  utcWednesday,
+  utcWednesdays,
+  utcThursday,
+  utcThursdays,
+  utcFriday,
+  utcFridays,
+  utcSaturday,
+  utcSaturdays,
+  utcMonth,
+  utcMonths,
+  utcYear,
+  utcYears
+} from "d3-time";
+
+export {
+  format,
+  formatPrefix,
+  formatLocale,
+  formatDefaultLocale,
+  formatSpecifier,
+  precisionFixed,
+  precisionPrefix,
+  precisionRound
+} from "d3-format";
+
+export {
+  timeFormat,
+  timeParse,
+  utcFormat,
+  utcParse,
+  isoFormat,
+  isoParse,
+  timeFormatLocale,
+  timeFormatDefaultLocale
+} from "d3-time-format";
+
 
 export {
   entries,
